ee43d11666f7ef864342f9e9b264ae53f8fd0d5e
mmelendez3 <mele0019@gmail.com>
2021-11-18 13:47:47 -0600

    feature/homepage-view

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path'); //used this to connect middleware path.join below to get the CSS file
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); //used this to connect CSS file in public folder
 
 // turn on routes
 app.use(routes);
@@ -15,3 +18,10 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+//set up handlebars as template engine of choice
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
